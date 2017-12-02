283156970a80baed5f86a46533af460c580f35b8
fjnw <faizanjamal2018@u.northwestern.edu>
2017-12-02 13:16:02 -0600

    heroku set up

--- server6.js
+++ server6.js
@@ -1,3 +1,6 @@
+
+
+
 // Dependencies
 // =============================================================
 var express = require("express");
@@ -7,7 +10,8 @@ var path = require("path");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = 3000;
+var PORT = process.env.PORT || 3000;
+
 
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.json());
